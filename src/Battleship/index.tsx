115ac851ac8a9c0769cc63e2dba13768532f0f24
David Dubliakov <dubliakov1@gmail.com>
2021-05-14 09:08:46 +0300

    added default field

--- src/Battleship/index.tsx
+++ src/Battleship/index.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+const DEFAULT_FIELDS = [
+  ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"],
+  ["1", null, null, null, null, null, null, null, null, null, null],
+  ["2", null, null, null, null, null, null, null, null, null, null],
+  ["3", null, null, null, null, null, null, null, null, null, null],
+  ["4", null, null, null, null, null, null, null, null, null, null],
+  ["5", null, null, null, null, null, null, null, null, null, null],
+  ["6", null, null, null, null, null, null, null, null, null, null],
+  ["7", null, null, null, null, null, null, null, null, null, null],
+  ["8", null, null, null, null, null, null, null, null, null, null],
+  ["9", null, null, null, null, null, null, null, null, null, null],
+  ["10", null, null, null, null, null, null, null, null, null, null],
+];
 
 const Battleship = () => {
+  const [personFields, setPersonFields] = useState(DEFAULT_FIELDS);
+  const [computerFields, setComputerFields] = useState(DEFAULT_FIELDS);
+
   return <div className="wrapper">Battleship</div>;
 };
 
